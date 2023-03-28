7ad268ae656baccbfcfcf43e1d67290f87d18337
alicia coquerant <alicia.coquerant@gmail.com>
2023-03-28 15:54:49 +0200

    added first component and Bulbasaur

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="Picture of Bulbasaur, a cute start Pokemon"
+      />
+      <figcaption>Bulbasaur</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;

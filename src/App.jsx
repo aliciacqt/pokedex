7e3bdfbee5e8d7d871c9b8566d0bf930377aaec7
alicia coquerant <alicia.coquerant@gmail.com>
2023-03-29 11:00:58 +0200

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import PokemonCard from "./components/PokemonCard.jsx";
 import "./App.css";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemonList={pokemonList} />
     </div>
   );
 }
